cdea6633616f03dcf1cb061a70573dcd077a1240
raunakpurohit <raunak.wscubetech@gmail.com>
2026-03-31 23:31:36 +0530

    text and container implemented

--- lib/example.dart
+++ lib/example.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-
-
 void main() {
   runApp(MyApp());
 }
@@ -12,7 +10,7 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: HomePage()
+        home: HomePage(),
     );
   }
 
@@ -33,8 +31,69 @@ class HomePage extends StatelessWidget{
           }, icon: Icon(Icons.more_vert))
         ],
       ),
-      body: Text('Hi, Welcome'),
+      body: Container(
+        margin: EdgeInsets.only(top: 11, left: 11),
+        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 5),
+        color: Colors.amber,
+        child: Text("Hi, Welcome to Flutter",
+          style: TextStyle(
+            fontSize: 34,
+            color: Colors.blue,
+            fontWeight: FontWeight.bold,
+            // fontStyle: FontStyle.italic,
+            // backgroundColor: Colors.orange,
+            // decoration: TextDecoration.lineThrough,
+            // decorationColor: Colors.pink,
+            // decorationThickness: 1,
+            // decorationStyle: TextDecorationStyle.double,
+            // letterSpacing: 11,
+            // wordSpacing: 11,
+            // fontFamily: 'cursive'
+          ),
+          overflow: TextOverflow.ellipsis,
+          maxLines: 3,
+        ),
+      )
     );
   }
 
 }
+
+///Text("Hi, Welcome to Flutter",
+//           style: TextStyle(
+//           fontSize: 34,
+//           color: Colors.blue,
+//           fontWeight: FontWeight.bold,
+//           // fontStyle: FontStyle.italic,
+//           // backgroundColor: Colors.orange,
+//           // decoration: TextDecoration.lineThrough,
+//           // decorationColor: Colors.pink,
+//           // decorationThickness: 1,
+//           // decorationStyle: TextDecorationStyle.double,
+//           // letterSpacing: 11,
+//           // wordSpacing: 11,
+//           // fontFamily: 'cursive'
+//         ),
+//           overflow: TextOverflow.ellipsis,
+//           maxLines: 3,
+//         )
+
+///style: GoogleFonts.unifrakturCook(
+//           fontSize: 34,
+//           color: Colors.blue,
+//           fontWeight: FontWeight.bold,
+//           // fontStyle: FontStyle.italic,
+//           // backgroundColor: Colors.orange,
+//           // decoration: TextDecoration.lineThrough,
+//           // decorationColor: Colors.pink,
+//           // decorationThickness: 1,
+//           // decorationStyle: TextDecorationStyle.double,
+//           // letterSpacing: 11,
+//           // wordSpacing: 11,
+//           // fontFamily: 'cursive'
+//         ),),
+
+///Align(
+//        alignment: Alignment(-0.5,-0.5),
+//         child: Text('Hi, Welcome'),
+//       ),
