9d50573f16f67519defb56ce0a545f8f3c36d732
raunakpurohit <raunak.wscubetech@gmail.com>
2026-04-07 23:49:19 +0530

    textfield, button, row and column implemented

--- lib/example.dart
+++ lib/example.dart
@@ -12,49 +12,198 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
+
+  TextEditingController nameController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
       appBar: AppBar(
         backgroundColor: Colors.orange,
         foregroundColor: Colors.white,
         title: Text('Home'),
-        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.more_vert))],
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.more_vert))],
       ),
-      body: Center(
-        child: Container(
-          width: 200,
-         height: 200,
-         decoration: BoxDecoration(
-           ///borderRadius: BorderRadius.circular(100),
-           shape: BoxShape.circle,
-           color: Colors.amber,
-           image: DecorationImage(
-               image: NetworkImage("https://m.media-amazon.com/images/M/MV5BMmU1YWU1NmMtMjAyMi00MjFiLWFmZmUtOTc1ZjI5ODkxYmQyXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg"),
-             fit: BoxFit.cover
-           )
-         ),
-        ),
-      ),
+      body: Container(
+        color: Colors.grey,
+        child: Row(
+           mainAxisAlignment: MainAxisAlignment.spaceAround,
+          /// crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                width: 50,
+                height: 50,
+                color: Colors.amber,
+              ),
+              Container(
+                width: 50,
+                height: 200,
+                color: Colors.blue,
+                child: Column(
+                  children: [
+                    Container(
+                      width: 50,
+                      height: 50,
+                      color: Colors.amber,
+                      child: Row(
+                        children: [
+                          Container(
+                            width: 25,
+                            height: 25,
+                            color: Colors.red,
+                          ),
 
-      ///Center(
-      //         child: Container(
-      //           child: ClipOval(
-      //             ///borderRadius: BorderRadius.circular(100),
-      //             child: Image.network(
-      //               "https://m.media-amazon.com/images/M/MV5BMmU1YWU1NmMtMjAyMi00MjFiLWFmZmUtOTc1ZjI5ODkxYmQyXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
-      //               width: 200,
-      //               height: 200,
-      //               fit: BoxFit.cover,
-      //             ),
-      //           ),
-      //         ),
-      //       ),
-      ///Image.asset("assets/images/ic_banner.png", width: 100, height: 100),
+                          Container(
+                            width: 25,
+                            height: 25,
+                            color: Colors.purple,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: 50,
+                      height: 50,
+                      color: Colors.orange,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                width: 50,
+                height: 50,
+                color: Colors.orange,
+              ),
+              Container(
+                width: 50,
+                height: 50,
+                color: Colors.green,
+              ),
+            ],
+        ),
+      )
     );
   }
 }
+
+///Center(
+//         child: Center(
+//         child: InkWell(
+//           onTap: (){
+//             print('Tapped!!');
+//           },
+//           child: Container(
+//             width: 200,
+//            height: 200,
+//            decoration: BoxDecoration(
+//              ///borderRadius: BorderRadius.circular(100),
+//              shape: BoxShape.circle,
+//              color: Colors.amber,
+//              image: DecorationImage(
+//                  image: NetworkImage("https://m.media-amazon.com/images/M/MV5BMmU1YWU1NmMtMjAyMi00MjFiLWFmZmUtOTc1ZjI5ODkxYmQyXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg"),
+//                fit: BoxFit.cover
+//              )
+//            ),
+//           ),
+//         ),
+//       ),
+//       )
+
+/// SizedBox(
+//           width: 200,
+//           child: IconButton(onPressed: (){}, icon: Icon(Icons.add))
+//         ),
+
+///OutlinedButton(
+//               onPressed: (){}, child: Text('OnTap')),
+
+///ElevatedButton(
+//             style: ElevatedButton.styleFrom(
+//               ///maximumSize: Size(300, 50),
+//               ///minimumSize: Size(200, 50),
+//               elevation: 3,
+//               backgroundColor: Colors.amber,
+//               foregroundColor: Colors.white,
+//               shape: RoundedRectangleBorder(
+//                 borderRadius: BorderRadius.zero,
+//                 /*side: BorderSide(
+//                   width: 1,
+//                   color: Colors.black,
+//                 )*/
+//               )
+//             ),
+//             onPressed: (){
+// 
+//             },
+//             child: Text('On'),
+//           )
+
+///Padding(
+//         padding: const EdgeInsets.all(11.0),
+//         child: TextField(
+//           obscureText: true,
+//           ///obscuringCharacter: "*",
+//           keyboardType: TextInputType.emailAddress,
+//           controller: nameController,
+//           decoration: InputDecoration(
+//             filled: true,
+//             fillColor: Colors.green,
+//             ///label: Icon(Icons.add),
+//             labelText: "Password",
+//             hintText: "Enter your password here..",
+//             prefixIcon: Icon(Icons.account_circle_outlined),
+//             ///prefixText: "Mr./Mrs.",
+//             ///suffixText: "@gmail.com",
+//             suffixIcon: Icon(Icons.visibility_off_outlined),
+//             enabledBorder: OutlineInputBorder(
+//               borderRadius: BorderRadius.circular(21),
+//               borderSide: BorderSide(
+//                 width: 1,
+//                 color: Colors.green,
+//               )
+//             ),
+//             focusedBorder: OutlineInputBorder(
+//                 borderRadius: BorderRadius.circular(21),
+//                 borderSide: BorderSide(
+//                   width: 2,
+//                   color: Colors.orange,
+//                 )
+//             ),
+//           ),
+//         ),
+//       )
+
+///Center(
+//         child: Container(
+//           width: 200,
+//          height: 200,
+//          decoration: BoxDecoration(
+//            ///borderRadius: BorderRadius.circular(100),
+//            shape: BoxShape.circle,
+//            color: Colors.amber,
+//            image: DecorationImage(
+//                image: NetworkImage("https://m.media-amazon.com/images/M/MV5BMmU1YWU1NmMtMjAyMi00MjFiLWFmZmUtOTc1ZjI5ODkxYmQyXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg"),
+//              fit: BoxFit.cover
+//            )
+//          ),
+//         ),
+//       ),
+//
+//       ///Center(
+//       //         child: Container(
+//       //           child: ClipOval(
+//       //             ///borderRadius: BorderRadius.circular(100),
+//       //             child: Image.network(
+//       //               "https://m.media-amazon.com/images/M/MV5BMmU1YWU1NmMtMjAyMi00MjFiLWFmZmUtOTc1ZjI5ODkxYmQyXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+//       //               width: 200,
+//       //               height: 200,
+//       //               fit: BoxFit.cover,
+//       //             ),
+//       //           ),
+//       //         ),
+//       //       ),
+//       ///Image.asset("assets/images/ic_banner.png", width: 100, height: 100),
 
 ///Center(
 //         child: Card(
